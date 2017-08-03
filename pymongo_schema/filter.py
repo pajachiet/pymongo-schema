a54332c16fb209f59e7e578ef69dbba6b981ad33
JulieRossi <julierossi06@gmail.com>
2017-08-03 17:47:53 +0200

    filter: pep8 & small refactoring

--- pymongo_schema/filter.py
+++ pymongo_schema/filter.py
@@ -1,14 +1,17 @@
 # coding: utf8
 from copy import deepcopy
 import logging
+
 logger = logging.getLogger(__name__)
 
-PRESENT_VALUE = 'present'  # Schema of fields to include is based on keys of a dict. PRESENT_VALUE is used as the value for thoses keys...
+# Schema of fields to include is based on keys of a dict.
+# PRESENT_VALUE is used as the value for thoses keys...
+PRESENT_VALUE = 'present'
 
 
 def filter_mongo_schema_namespaces(mongo_schema, namespaces_dict):
-    """ Filter the schema with namespaces 
-    
+    """ Filter the schema with namespaces
+
     :param mongo_schema: dict
     :param namespaces_dict: dict
     :return filtered_schema: dict
@@ -22,7 +25,8 @@ def filter_mongo_schema_namespaces(mongo_schema, namespaces_dict):
         db, collection = namespace.split('.', 1)
 
         if db not in mongo_schema:
-            logger.warning('WARNING : Database {} is supposed to be filtered, but is not present in mongo schema'.format(db))
+            logger.warning('WARNING : Database %s is supposed to be filtered, but is not present '
+                           'in mongo schema', db)
             continue
         database_schema = mongo_schema[db]
 
@@ -33,46 +37,47 @@ def filter_mongo_schema_namespaces(mongo_schema, namespaces_dict):
 
         for collection in collection_list:
             if collection not in database_schema:
-                logger.warning('WARNING : Collection {} is supposed to be filtered from database {}, but is not present in mongo schema'.format(collection, db))
+                logger.warning('WARNING : Collection %s is supposed to be filtered from database '
+                               '%s, but is not present in mongo schema', collection, db)
                 continue
             collection_schema = database_schema[collection]
 
             if filt is True:
-                logger.info("Include the whole collection" + collection)
+                logger.info("Include the whole collection %s", collection)
                 filtered_schema[db][collection] = collection_schema
             else:
                 if 'excludeFields' in filt:
-                    logger.info("Exclude fields from collection " + collection)
-                    exclude_fields = filt['excludeFields']
-                    filtered_schema[db][collection] = exclude_fields_from_collection_schema(exclude_fields, collection_schema)
+                    logger.info("Exclude fields from collection %s", collection)
+                    filtered_schema[db][collection] = exclude_fields_from_collection_schema(
+                        filt['excludeFields'], collection_schema)
                 elif 'includeFields' in filt:
-                    logger.info("Include fields from collection " + collection)
-                    include_fields = filt['includeFields']
-                    filtered_schema[db][collection] = include_fields_from_collection_schema(include_fields, collection_schema)
+                    logger.info("Include fields from collection %s", collection)
+                    filtered_schema[db][collection] = include_fields_from_collection_schema(
+                        filt['includeFields'], collection_schema)
                 else:
-                    raise NotImplementedError('unknown option, not implemented : {}'.format(filt.keys()))
+                    raise NotImplementedError('unknown option, not implemented : %s', filt.keys())
 
-    for db in filtered_schema.keys():
+    for db in filtered_schema:
         if not filtered_schema[db]:
             del filtered_schema[db]
     return filtered_schema
 
 
 def init_filtered_schema(namespaces_dict):
-    """ Initialize filtered_schema dict for databases present in namespace 
-    
+    """ Initialize filtered_schema dict for databases present in namespace
+
     :param namespaces_dict: dict
     :return filtered_schema: dict
     """
     filtered_schema = dict()
-    for namespace in namespaces_dict.keys():
-        db, collection = namespace.split('.', 1)
+    for namespace in namespaces_dict:
+        db = namespace.split('.')[0]
         filtered_schema[db] = dict()
     return filtered_schema
 
 
 def include_fields_from_collection_schema(include_fields_list, collection_schema):
-    """ Copy collection_schema, keeping only included fields 
+    """ Copy collection_schema, keeping only included fields.
     >>> collection_schema = {\
         'object': {\
             'field':'field_schema',\
@@ -96,10 +101,10 @@ def include_fields_from_collection_schema(include_fields_list, collection_schema
 
 
 def include_fields_from_object_schema(include_fields_dict, object_count_schema):
-    """ Add fields from include_fields_dict to object schema, with no return value.
+    """ Add fields from include_fields_dict to object schema.
 
     :param include_fields_dict: dict
-    :param object_count_schema: dict 
+    :param object_count_schema: dict
     """
     object_schema_filtered = {k: v for k, v in object_count_schema.iteritems() if k != 'object'}
     object_schema_filtered['object'] = {}
@@ -107,7 +112,7 @@ def include_fields_from_object_schema(include_fields_dict, object_count_schema):
     object_schema = object_count_schema['object']
     for field, value in include_fields_dict.iteritems():
         if field not in object_schema:
-            logger.warn("WARNING: Field '{}' is present in includeFields but not in schema".format(field))
+            logger.warn("WARNING: Field '%s' is present in includeFields but not in schema", field)
             continue
 
         if value is PRESENT_VALUE:
@@ -115,14 +120,15 @@ def include_fields_from_object_schema(include_fields_dict, object_count_schema):
         else:
             subfield_dict = value
             field_schema = object_schema[field]
-            object_schema_filtered['object'][field] = include_fields_from_object_schema(subfield_dict, field_schema)
+            object_schema_filtered['object'][field] = include_fields_from_object_schema(
+                subfield_dict, field_schema)
 
     return object_schema_filtered
 
 
 def exclude_fields_from_collection_schema(exclude_fields_list, collection_schema):
     """ Deep copy collection_schema and exclude fields from it.
-    
+
     >>> collection_schema = {\
         'object': {\
             'field':'field_schema',\
@@ -140,23 +146,24 @@ def exclude_fields_from_collection_schema(exclude_fields_list, collection_schema
 
     :param exclude_fields_list: list
     :param collection_schema: dict
-    :return collection_schema_copied_filtered: dict 
+    :return collection_schema_copied_filtered: dict
     """
     collection_schema_copied_filtered = deepcopy(collection_schema)
     exclude_fields_dict = field_list_to_dict(exclude_fields_list)
-    exclude_fields_from_object_schema(exclude_fields_dict, collection_schema_copied_filtered['object'])
+    exclude_fields_from_object_schema(exclude_fields_dict,
+                                      collection_schema_copied_filtered['object'])
     return collection_schema_copied_filtered
 
 
 def exclude_fields_from_object_schema(exclude_fields_dict, object_schema):
     """ Exclude fields from object schema, with no return value.
-    
+
     :param exclude_fields_dict: dict
-    :param object_schema: dict 
+    :param object_schema: dict
     """
     for field, value in exclude_fields_dict.iteritems():
         if field not in object_schema:
-            logger.warn("WARNING: Field '{}' is present in excludeFields, but not in schema".format(field))
+            logger.warn("WARNING: Field '%s' is present in excludeFields, but not in schema", field)
             continue
 
         if value is PRESENT_VALUE:
@@ -169,10 +176,10 @@ def exclude_fields_from_object_schema(exclude_fields_dict, object_schema):
 
 def field_list_to_dict(field_list):
     """ Transform a field list to a recursive field dictionary
-    
+
     >>> field_list_to_dict(['field', 'sub.field'])
     {'field': 'present', 'sub': {'field': 'present'}}
-    
+
     :param field_list: list
     :return field_dict: dict
     """
@@ -183,10 +190,10 @@ def field_list_to_dict(field_list):
 
 
 def add_field_to_dict(field, fields_dict):
-    """ Recursively add a field and its subfield to a field dictionnary. No return value.
-        
+    """ Recursively add a field and its subfield to a field dictionary. No return value.
+
     :param field: str
-    :param fields_dict: dict 
+    :param fields_dict: dict
     """
     if '.' not in field:
         fields_dict[field] = PRESENT_VALUE
@@ -196,8 +203,6 @@ def add_field_to_dict(field, fields_dict):
             fields_dict[parent_field] = dict()
             add_field_to_dict(subfield, fields_dict[parent_field])
         else:
-            if not fields_dict[parent_field] is PRESENT_VALUE:  # Test if parent field is not already forced to be included
+            if not fields_dict[parent_field] is PRESENT_VALUE:
+                # Test if parent field is not already forced to be included
                 add_field_to_dict(subfield, fields_dict[parent_field])
-
-
-
