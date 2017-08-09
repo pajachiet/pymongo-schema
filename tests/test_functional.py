a1f1cfe014aa9c925191947f21c93b731ca222c7
JulieRossi <julierossi06@gmail.com>
2017-08-09 15:19:59 +0200

    extract: pep8

---
+++ tests/test_functional.py
@@ -0,0 +1,26 @@
+import json
+import os
+import unittest
+
+from pymongo import MongoClient
+
+from pymongo_schema.extract import extract_pymongo_client_schema
+from pymongo_schema.tosql import mongo_schema_to_mapping
+
+TEST_DIR = os.path.dirname(__file__)
+
+
+class TestFunctinonal(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.pymongo_client = MongoClient()
+
+    def test_from_mongo_to_mapping(self):
+        with open(os.path.join(TEST_DIR, "resources", "input", "mapping.json")) as f:
+            exp_mapping = json.load(f)
+        mongo_schema = extract_pymongo_client_schema(self.pymongo_client,
+                                                     database_names='test_db',
+                                                     collection_names='test_col')
+
+        mapping = mongo_schema_to_mapping(mongo_schema)
+        self.assertEqual(mapping, exp_mapping)
