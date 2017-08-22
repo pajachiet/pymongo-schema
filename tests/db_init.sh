2cd3461b3641a3c0101df5d903e81344fe6dc08a
JulieRossi <julierossi06@gmail.com>
2017-08-22 10:29:16 +0200

    improve functional test

--- tests/db_init.sh
+++ tests/db_init.sh
@@ -10,8 +10,19 @@ if [ ! -d "$db_directory" ]; then
 fi
 
 # Start mongod as a service
-mongod --fork --dbpath $db_directory --logpath "$db_directory/mongodb.log"
+#mongod --fork --dbpath $db_directory --logpath "$db_directory/mongodb.log"
 
 # Import test dataset
-mongoimport --db test_db --collection test_col --drop --file "tests/test_db.json"
+DB_DIRS=tests/resources/functional/input_dbs/*
+for db in ${DB_DIRS}
+do
+echo "Processing $db directory"
+DB_NAME=$(basename ${db})
+for f in $(ls ${db})
+do
+  echo "Processing ${db}/${f} file"
+  FILE_NAME=$(basename ${f})
+  mongoimport --db ${DB_NAME} --collection "${FILE_NAME/.json/}" --drop --file ${db}/${f}
+done
+done
 
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -35,6 +35,16 @@ def test00_from_mongo_to_mapping(pymongo_client):
     assert mapping == exp_mapping
 
 
+def test00_from_mongo_to_mapping_long(pymongo_client):
+    with open(os.path.join(TEST_DIR, 'resources', "functional", "exp_mapping.json")) as f:
+        exp_mapping = json.load(f)
+    mongo_schema = extract_pymongo_client_schema(pymongo_client,
+                                                 database_names=['test_db', 'test_db1', 'test_db2'])
+
+    mapping = mongo_schema_to_mapping(mongo_schema)
+    assert mapping == exp_mapping
+
+
 def test01_extract():
     output = os.path.join(TEST_DIR, "output_fctl_schema.json")
     expected_file = os.path.join(TEST_DIR, 'resources', 'expected', 'schema.json')
